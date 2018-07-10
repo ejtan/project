5fd4575f3785e564827c9b324dbce4e60fa264ff
Eric Tan <eric@thinkpad.localdomain>
2018-07-10 15:42:22 -0700

    Added constructor

--- other/dir_size/cmd_options.cpp
+++ other/dir_size/cmd_options.cpp
@@ -3,4 +3,28 @@
 
 cmd_options::cmd_options(int argc, char **const argv)
 {
+    // Allowed flags
+    program.add_options()
+        ("help,h", "Prints help message.")
+        ("sym,s", boost::program_options::bool_switch(&use_symlink), "Allow symbolic links.")
+        ("n", "Use nice number output (uses system locale).")
+        ("d", "Set max recursion depth allowed");
+
+    // Hidden input not printed in help message.
+    boost::program_options::options_description hidden;
+    hidden.add_options()
+        ("dir", boost::program_options::value<std::filesystem::path>(&root_dir), "Input directory");
+
+    // Take the directory input as a positional parameter
+    boost::program_options::positional_options_description dir_arg;
+    dir_arg.add("dir", -1);
+
+    // Group hidden and program options
+    boost::program_options::options_description cmd_line_input;
+    cmd_line_input.add(program).add(hidden);
+
+    // Store arguments to variables_map arg_values
+    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).
+            options(cmd_line_input).positional(dir_arg).run(), arg_values);
+    boost::program_options::notify(arg_values);
 }
--- other/dir_size/cmd_options.h
+++ other/dir_size/cmd_options.h
@@ -1,6 +1,8 @@
 #ifndef CMD_OPTIONS_H
 #define CMD_OPTIONS_H
 
+#include <filesystem>
+
 #include <boost/program_options.hpp>
 
 
@@ -12,6 +14,9 @@ class cmd_options
 {
     private:
         boost::program_options::variables_map arg_values;
+        boost::program_options::options_description program;
+        std::filesystem::path root_dir;
+        bool use_symlink;
 
     public:
         cmd_options(int argc, char **const argv);
