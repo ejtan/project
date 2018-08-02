37d19a3fe3fe87718ac1906b3c0479d7651e1d75
Eric Tan <eric@thinkpad.localdomain>
2018-08-02 12:41:12 -0700

    Initial commit

---
+++ algorithm/sorting/psrs/psrs/mpi_utility.h
@@ -0,0 +1,48 @@
+#ifndef MPI_UTILITY_H
+#define MPI_UTILITY_H
+
+
+namespace psrs {
+namespace utility {
+
+
+// message tags
+constexpr int data_tag = 1;
+
+// Functions
+
+/* blk_low()
+ * @INPUT: id = proc id
+ * @INPUT: p = number of procs
+ * @INPUT: n = number of elements
+ *
+ * Computes the low index of the block.
+ */
+int blk_low(int id, int p, int n) { return id * n / p; }
+
+
+/* blk_low()
+ * @INPUT: id = proc id
+ * @INPUT: p = number of procs
+ * @INPUT: n = number of elements
+ *
+ * Computes the high index of the block.
+ */
+int blk_high(int id, int p, int n) { return blk_low(id + 1, p, n) - 1; }
+
+
+/* blk_low()
+ * @INPUT: id = proc id
+ * @INPUT: p = number of procs
+ * @INPUT: n = number of elements
+ *
+ * Computes the size of the block.
+ */
+int blk_size(int id, int p, int n) { return blk_high(id, p, n) - blk_low(id, p, n) + 1; }
+
+
+}; // namespace utility
+}; // namespace psrs
+
+
+#endif
