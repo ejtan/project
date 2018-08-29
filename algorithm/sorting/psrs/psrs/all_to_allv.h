3a68cf9e882c4346fb12fe7a165b90f8c3e93dfa
Eric Tan <eric@thinkpad.attlocal.net>
2018-08-29 00:13:43 -0700

    Implamented support for serializable datatypes

--- algorithm/sorting/psrs/psrs/all_to_allv.h
+++ algorithm/sorting/psrs/psrs/all_to_allv.h
@@ -5,12 +5,15 @@
 #ifndef MPI_ALLTOALLV
 #define MPI_ALLTOALLV
 
+#include <vector>
 #include <numeric>
 #include <mpi.h>
 
 #include <boost/mpi/communicator.hpp>
 #include <boost/mpi/datatype.hpp>
 #include <boost/mpi/exception.hpp>
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/allocator.hpp>
 
 
 namespace psrs {
@@ -31,6 +34,67 @@ void all_to_allv_mpi_type_impl(const boost::mpi::communicator &comm,
 }
 
 
+template <typename T>
+void all_to_allv_general_impl(const boost::mpi::communicator &comm,
+        const T *in_values, const int *in_count, const int *in_disp,
+        T *out_values, const int *out_count, const int *out_disp)
+{
+    int p = comm.size();
+    int rank = comm.rank();
+
+    // Prepare data to send out
+    std::vector<int> send_count(p), send_disp(p);
+    std::vector<char, boost::mpi::allocator<char> > outgoing;
+
+    int skip = 0;
+    for (int dest = 0; dest < p; dest++) {
+        send_disp[dest] = outgoing.size();
+
+        if (dest != rank) {
+            boost::mpi::packed_oarchive oa(comm, outgoing);
+            for (int i = 0; i < in_count[dest]; i++)
+                oa << in_values[skip + i];
+        } // Pack data if needed
+
+        skip += in_count[dest];
+        send_count[dest] = outgoing.size() - send_disp[dest];
+    } // Pack outgoing data
+
+    // Prepare incoming data
+    std::vector<int> recv_count(p), recv_disp(p);
+    boost::mpi::all_to_all(comm, send_count, recv_count);
+
+    int sum = 0;
+    for (int src = 0; src < p; src++) {
+        recv_disp[src] = sum;
+        sum += recv_count[src];
+    } // Compute recv_disp
+
+    std::vector<char, boost::mpi::allocator<char> > incoming(sum > 0 ? sum : 1);
+
+    // Incase outgoing vector is empty, put a value
+    if (outgoing.empty())
+        outgoing.push_back(0);
+
+    // Perform data swap on the buffers
+    BOOST_MPI_CHECK_RESULT(MPI_Alltoallv, (&outgoing[0], &send_count[0], &send_disp[0],
+                MPI_PACKED, &incoming[0], &recv_count[0], &recv_disp[0], MPI_PACKED, comm));
+
+    // Unpack buffer
+    for (int proc = 0; proc < p; proc++) {
+        if (proc != rank) {
+            boost::mpi::packed_iarchive ia(comm, incoming,
+                    boost::archive::no_header, recv_disp[proc]);
+            for (int i = 0; i < out_count[proc]; i++)
+                ia >> out_values[out_disp[proc] + i];
+        } else {
+            std::copy(in_values + in_disp[proc], in_values + in_disp[proc] + in_count[proc],
+                    out_values + out_disp[proc]);
+        } // Unpack data depending on process rank
+    } // Loop over proc's data
+}
+
+
 }; // namespace detail
 
 
@@ -41,7 +105,8 @@ void all_to_allv_mpi_type_impl(const boost::mpi::communicator &comm,
  * @INPUT: out_values = output vector
  * @INPUT: out_values, out_count = count and displancement for output vector
  *
- * Wrapper for MPI_Alltoallv. Currently only supports MPI built in types
+ * Wrapper for MPI_Alltoallv. Supports both MPI_Datatype and serializable datatypes using
+ * the Boost.Serialization library.
  */
 template <typename T>
 inline void all_to_allv(const boost::mpi::communicator &comm, const std::vector<T> &in_values,
@@ -57,6 +122,9 @@ inline void all_to_allv(const boost::mpi::communicator &comm, const std::vector<
     if constexpr(boost::mpi::is_mpi_datatype<T>())
         detail::all_to_allv_mpi_type_impl(comm, &in_values[0], in_count.data(), in_disp.data(),
             &out_values[0], out_count.data(), out_disp.data());
+    else
+        detail::all_to_allv_general_impl(comm, &in_values[0], in_count.data(), in_disp.data(),
+                &out_values[0], out_count.data(), out_disp.data());
 }
 
 
